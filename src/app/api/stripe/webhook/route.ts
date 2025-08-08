2653942eea3af94f4a6bd54b9b609745e25a250a
Matheus Santos <maatheusalberto@hotmail.com>
2025-08-08 18:55:52 -0300

    route api webhook stripe

---
+++ src/app/api/stripe/webhook/route.ts
@@ -0,0 +1,42 @@
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+import { db } from "@/db";
+import { orderTable } from "@/db/schema";
+
+export const POST = async (request: NextRequest) => {
+  //acompanhar webhook: stripe listen --forward-to localhost:3000/api/stripe/webhook
+
+  const signature = request.headers.get("stripe-signature");
+
+  if (!signature || !process.env.STRIPE_WEBHOOK_SECRET) {
+    return NextResponse.error();
+  }
+
+  const text = await request.text();
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET,
+  );
+
+  if (event.type === "checkout.session.completed") {
+    const checkoutSession = event.data.object as Stripe.Checkout.Session;
+    const orderId = checkoutSession.metadata?.orderId;
+
+    if (!orderId) {
+      return NextResponse.error();
+    }
+
+    await db
+      .update(orderTable)
+      .set({
+        status: "paid",
+      })
+      .where(eq(orderTable.id, orderId));
+  }
+
+  return NextResponse.json({ received: true }, { status: 200 });
+};
